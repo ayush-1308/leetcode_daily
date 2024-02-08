45e3e145e53512a48f7a9781a42ddf5f02ad4043
ayush-1308 <ayushsinghlts123@gmail.com>
2024-02-08 13:50:49 +0530

    Leetcode_279

---
+++ Leetcode_279.java
@@ -0,0 +1,17 @@
+public class Leetcode_279 {
+  public static void main(String[] args) {
+    System.out.println(numSquares(12));
+    System.out.println(numSquares(13));
+  }
+
+  public static int numSquares(int n) {
+    int[] dp = new int[n + 1];
+    for (int i = 1; i <= n; i++) {
+      dp[i] = i;
+      for (int j = 1; j * j <= i; j++) {
+        dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+      }
+    }
+    return dp[n];
+  }
+}
