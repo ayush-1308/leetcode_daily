56b77ce1b3ed743bf7a8f154e24460444021548b
Ayush <ayushsinghlts123@gmail.com>
2024-09-23 22:48:26 +0530

    Learning and Revising LinkedList

---
+++ Linked_List/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+
+        list.insertFirst(9);
+        list.insertFirst(6);
+        list.insertFirst(92);
+        list.insertFirst(5);
+        list.insertFirst(1);
+        list.insertLast(10);
+
+        list.display();
+    }
+}
