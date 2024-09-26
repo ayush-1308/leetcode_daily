60bc83c800252b378bc4b778c97d7e1ce403c0e6
Ayush <ayushsinghlts123@gmail.com>
2024-09-26 20:55:13 +0530

    double linked list

--- Linked_List/DLL.java
+++ Linked_List/DLL.java
@@ -13,13 +13,54 @@ public void insertFirst(int val) {
     head = node;
 }
 
+public void insertLast(int val) {
+    Node node = new Node(val);
+    Node last = null;
+    while(head.next!= null){
+        head = node.next;
+        last = head;
+    }
+
+    if(last == null){
+        head = node;
+    }else {
+        last.next= node;
+        node.prev = last;
+    }
+}
+
+
+/* if tail is given */
+
+// public void insertLast(int val) {
+//     Node node = new Node(val);
+//     if (head == null) {
+//         node.prev = null;
+//         head = node;
+//         return;
+//     }
+//     Node tail = head;
+//     tail.next = node;
+//     node = tail;
+//     node.next = null;
+// }
+
 public void display() {
     Node node = head;
+    Node last = null;
     while(node!= null) {
         System.out.print(node.val+ " --> ");
+        last = node;
         node = node.next;
     }
     System.out.println("END");
+
+    System.out.print("Printing in reverse order");
+    while(last!= null){
+        System.out.println(last.val + " <-- ");
+        last = last.prev;
+    }
+    System.out.println("START");
 
 }
 
